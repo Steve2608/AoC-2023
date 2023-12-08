1bfa5476d3896216475aac0f52542cee458a4881
Stefan Brandl <steve260897@gmail.com>
2023-12-08 16:48:52 +0100

    day08 in python

---
+++ day08/day08.py
@@ -0,0 +1,48 @@
+import itertools as it
+import math
+from typing import Callable
+
+from timing_util import Timing
+
+
+def get_data(content: str) -> tuple[str, dict[str, tuple[str, str]]]:
+    directions, lines = content.split("\n\n")
+    # node = (left, right)
+    # ABC = (DEF, GHI)
+    nodes = {line[:3]: (line[7:10], line[12:15]) for line in lines.split("\n")}
+
+    return directions, nodes
+
+
+def follow_path(curr: str, directions: str, nodes: dict[str, tuple[str, str]], is_end: Callable[[str], bool]):
+    for i, direction in enumerate(it.cycle(directions)):
+        if is_end(curr):
+            return i
+
+        # coerce boolean to index: false -> left; true -> right
+        curr = nodes[curr][direction == "R"]
+
+
+def part1(data: tuple[str, dict[str, tuple[str, str]]]) -> int:
+    def is_end(node: str) -> bool:
+        return node == "ZZZ"
+
+    directions, nodes = data
+    return follow_path("AAA", directions, nodes, is_end)
+
+
+def part2(data: tuple[str, dict[str, tuple[str, str]]]) -> int:
+    def is_end(node: str) -> bool:
+        return node[-1] == "Z"
+
+    directions, nodes = data
+    return math.lcm(*(follow_path(start, directions, nodes, is_end) for start in nodes if start[-1] == "A"))
+
+
+if __name__ == "__main__":
+    with Timing("Elapsed time: "):
+        with open("inputs/day08.txt") as in_file:
+            data = get_data(in_file.read().strip())
+
+        print(f"part1: {part1(data)}")
+        print(f"part2: {part2(data)}")
---
+++ day08/day08_test.py
@@ -0,0 +1,54 @@
+import unittest
+
+from day08 import get_data, part1, part2
+
+
+class AoCTest(unittest.TestCase):
+    def __init__(self, methodName: str = "runTest"):
+        super().__init__(methodName)
+        self.data1 = get_data(
+            r"""RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)"""
+        )
+
+        self.data2 = get_data(
+            r"""LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)"""
+        )
+
+        self.data3 = get_data(
+            r"""LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)"""
+        )
+
+    def test_part1(self) -> None:
+        p1 = part1(self.data1)
+        self.assertEqual(2, p1)
+        p1 = part1(self.data2)
+        self.assertEqual(6, p1)
+
+    def test_part2(self) -> None:
+        p2 = part2(self.data3)
+        self.assertEqual(6, p2)
+
+
+if __name__ == "__main__":
+    unittest.main()

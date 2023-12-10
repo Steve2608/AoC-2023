87b507ba304629cb20118c853a4ffd626d084308
Stefan Brandl <steve260897@gmail.com>
2023-12-10 16:59:26 +0100

    day10 in Python

---
+++ day10/day10.py
@@ -0,0 +1,219 @@
+import collections
+
+from timing_util import Timing
+
+
+def get_data(content: str) -> tuple[tuple[int, int], list[list[str]]]:
+    grid = [list(line) for line in content.split("\n")]
+    for x, line in enumerate(grid):
+        if "S" in line:
+            y = line.index("S")
+            line[y] = starting_pipe(start := (x, y), grid)
+            return start, grid
+
+
+def starting_pipe(start: tuple[int, int], grid: list[list[str]]):
+    x, y = start
+    connect_up = True if (x - 1 >= 0 and grid[x - 1][y]) in {"|", "F", "7"} else False
+    connect_down = True if (x + 1 < len(grid) and grid[x + 1][y]) in {"|", "L", "J"} else False
+    connect_left = True if (y - 1 >= 0 and grid[x][y - 1]) in {"-", "L", "F"} else False
+    connect_right = True if (y + 1 < len(grid[x]) and grid[x][y + 1]) in {"-", "J", "7"} else False
+
+    match (connect_up, connect_down, connect_left, connect_right):
+        case (True, True, False, False):
+            return "|"
+        case (True, False, True, False):
+            return "J"
+        case (True, False, False, True):
+            return "L"
+        case (False, True, True, False):
+            return "7"
+        case (False, True, False, True):
+            return "F"
+        case (False, False, True, True):
+            return "-"
+        case _:
+            raise ValueError(f"Illegal pipe required for {(connect_up, connect_down, connect_left, connect_right)}")
+
+
+def is_connected(src: tuple[int, int], dst: tuple[int, int], grid: list[list[str]]) -> bool:
+    x_dst, y_dst = dst
+    dst_pipe = grid[x_dst][y_dst]
+    if dst_pipe == ".":
+        # can never connect to "."
+        return False
+
+    x_src, y_src = src
+    src_pipe = grid[x_src][y_src]
+
+    if x_src == x_dst:
+        # right
+        if y_src + 1 == y_dst:
+            return src_pipe in "-LF" and dst_pipe in "7J-"
+        # left
+        if y_src - 1 == y_dst:
+            return src_pipe in "7J-" and dst_pipe in "-LF"
+    elif y_src == y_dst:
+        # down
+        if x_src + 1 == x_dst:
+            return src_pipe in "F7|" and dst_pipe in "LJ|"
+        # up
+        if x_src - 1 == x_dst:
+            return src_pipe in "LJ|" and dst_pipe in "F7|"
+    return False
+
+
+def trace_path(start: tuple[int, int], grid: list[list[str]]) -> list[tuple[int, int]]:
+    visited = set()
+    path = []
+    curr = start
+    while curr not in visited:
+        visited.add(curr)
+        path.append(curr)
+
+        x, y = curr
+        for i, j in ((x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[i]):
+                continue
+
+            if (step := (i, j)) not in visited and is_connected(curr, step, grid):
+                curr = step
+                # we can only ever connect to one tile
+                break
+
+    return path
+
+
+def part1(data: tuple[tuple[int, int], list[list[str]]]) -> int:
+    start, grid = data
+    path = trace_path(start, grid)
+    return len(path) // 2
+
+
+def set_tile(x: int, y: int, tile: str, zoomed: list[list[str]]):
+    match tile:
+        case ".":
+            fill_in = [
+                [" ", " ", " "],
+                [" ", " ", " "],
+                [" ", " ", " "],
+            ]
+        case "|":
+            fill_in = [
+                [" ", "x", " "],
+                [" ", "x", " "],
+                [" ", "x", " "],
+            ]
+        case "-":
+            fill_in = [
+                [" ", " ", " "],
+                ["x", "x", "x"],
+                [" ", " ", " "],
+            ]
+        case "7":
+            fill_in = [
+                [" ", " ", " "],
+                ["x", "x", " "],
+                [" ", "x", " "],
+            ]
+        case "F":
+            fill_in = [
+                [" ", " ", " "],
+                [" ", "x", "x"],
+                [" ", "x", " "],
+            ]
+        case "J":
+            fill_in = [
+                [" ", "x", " "],
+                ["x", "x", " "],
+                [" ", " ", " "],
+            ]
+        case "L":
+            fill_in = [
+                [" ", "x", " "],
+                [" ", "x", "x"],
+                [" ", " ", " "],
+            ]
+        case _:
+            raise ValueError
+
+    for i, fill in enumerate(fill_in):
+        x_ = x * 3 + i
+        zoomed[x_][y * 3 : (y + 1) * 3] = fill
+
+
+def init_zoomed(grid: list[list[str]], path: set[tuple[int, int]]):
+    # replace every tile with a 3-by-3 "zoomed in" tile
+    zoomed = [[" " for _ in range(len(grid[0] * 3))] for _ in range(len(grid) * 3)]
+    for x, line in enumerate(grid):
+        for y, pipe in enumerate(line):
+            # if we're not on the path, just assume we're ground
+            if (x, y) in path:
+                set_tile(x, y, pipe, zoomed)
+    return zoomed
+
+
+def is_inside(
+    start: tuple[int, int],
+    zoomed: list[list[str]],
+    known_inside: set[tuple[int, int]],
+    known_outside: set[tuple[int, int]],
+) -> bool:
+    if start in known_inside:
+        return True
+    if start in known_outside:
+        return False
+
+    visited = set()
+    fringe = collections.deque([start])
+    while fringe:
+        curr = fringe.popleft()
+        if curr in visited:
+            continue
+        visited.add(curr)
+
+        x, y = curr
+        for i, j in ((x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)):
+            if i < 0 or i >= len(zoomed) or j < 0 or j >= len(zoomed[i]):
+                known_outside.update(visited)
+                return False
+
+            # cannot walk over pipes
+            if zoomed[i][j] == "x":
+                continue
+
+            if (step := (i, j)) not in visited:
+                fringe.append(step)
+
+    known_inside.update(visited)
+    return True
+
+
+def count_inside(grid: list[list[str]], zoomed: list[list[str]], path: set[tuple[int, int]]) -> int:
+    n = 0
+    known_outside = set()
+    known_inside = set()
+    for x, line in enumerate(grid):
+        for y, _ in enumerate(line):
+            if (x, y) not in path:
+                n += is_inside((x * 3 + 1, y * 3 + 1), zoomed, known_outside, known_inside)
+
+    return n
+
+
+def part2(data) -> int:
+    start, grid = data
+
+    path = set(trace_path(start, grid))
+    zoomed = init_zoomed(grid, path)
+
+    return count_inside(grid, zoomed, path)
+
+
+if __name__ == "__main__":
+    with Timing("Elapsed time: "):
+        with open("inputs/day10.txt") as in_file:
+            data = get_data(in_file.read().strip())
+
+        print(f"part1: {part1(data)}")
+        print(f"part2: {part2(data)}")
---
+++ day10/day10_test.py
@@ -0,0 +1,63 @@
+import unittest
+
+from day10 import get_data, part1, part2
+
+
+class AoCTest(unittest.TestCase):
+    def __init__(self, methodName: str = "runTest"):
+        super().__init__(methodName)
+        self.data1 = get_data(
+            r""".....
+.S-7.
+.|.|.
+.L-J.
+....."""
+        )
+        self.data2 = get_data(
+            r"""7-F7-
+.FJ|7
+SJLL7
+|F--J
+LJ.LJ"""
+        )
+
+        self.data3 = get_data(
+            r"""..........
+.S------7.
+.|F----7|.
+.||....||.
+.||....||.
+.|L-7F-J|.
+.|..||..|.
+.L--JL--J.
+.........."""
+        )
+
+        self.data4 = get_data(
+            r"""FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L"""
+        )
+
+    def test_part1(self) -> None:
+        p1 = part1(self.data1)
+        self.assertEqual(4, p1)
+        p1 = part1(self.data2)
+        self.assertEqual(8, p1)
+
+    def test_part2(self) -> None:
+        p2 = part2(self.data3)
+        self.assertEqual(4, p2)
+        p2 = part2(self.data4)
+        self.assertEqual(10, p2)
+
+
+if __name__ == "__main__":
+    unittest.main()

e52096b40e06800fd13796bb0703ffd3a40bb9a4
Stefan Brandl <steve260897@gmail.com>
2023-12-14 10:51:40 +0100

    improved Algorithm

--- day11/sol.go
+++ day11/sol.go
@@ -7,6 +7,10 @@ import (
 	"time"
 )
 
+type Point struct {
+	x, y int
+}
+
 func ReadFile(filename string) string {
 	bytes, err := os.ReadFile(filename)
 	if err != nil {
@@ -15,22 +19,22 @@ func ReadFile(filename string) string {
 	return strings.TrimSpace(string(bytes))
 }
 
-func ParseInput(data string) ([]int, []int, [][]int) {
+func ParseInput(data string) ([]int, []int, []Point) {
 	rows := make([]int, 0)
 	cols := make([]int, 0)
-	galaxies := make([][]int, 0)
+	galaxies := make([]Point, 0)
 	grid := strings.Split(data, "\n")
 	for i, line := range grid {
 		for j, val := range line {
 			if val == '#' {
-				galaxies = append(galaxies, []int{i, j})
+				galaxies = append(galaxies, Point{i, j})
 			}
 		}
 	}
 	for i := range grid {
 		append_row := true
 		for _, g := range galaxies {
-			if i == g[0] {
+			if i == g.x {
 				append_row = false
 				break
 			}
@@ -42,7 +46,7 @@ func ParseInput(data string) ([]int, []int, [][]int) {
 	for j := range grid[0] {
 		append_col := true
 		for _, g := range galaxies {
-			if j == g[1] {
+			if j == g.y {
 				append_col = false
 				break
 			}
@@ -69,47 +73,53 @@ func Max(a, b int) int {
 	return b
 }
 
-func ManhattanDistance(a, b, rows, cols []int, expansion int) int {
-	src_x, src_y := a[0], a[1]
-	dst_x, dst_y := b[0], b[1]
-
-	min_x := Min(src_x, dst_x)
-	max_x := Max(src_x, dst_x)
-	min_y := Min(src_y, dst_y)
-	max_y := Max(src_y, dst_y)
-
-	n_rows := 0
-	for _, row := range rows {
-		if min_x < row && row < max_x {
-			n_rows++
-		}
+func Abs(a int) int {
+	if a < 0 {
+		return -a
 	}
-
-	n_cols := 0
-	for _, col := range cols {
-		if min_y < col && col < max_y {
-			n_cols++
-		}
-	}
-
-	return (max_x - min_x) + (max_y - min_y) + (n_rows+n_cols)*expansion
+	return a
 }
 
-func solve(rows, cols []int, galaxies [][]int, expansion int) int {
+func ManhattanDistance(a, b Point) int {
+	return Abs(a.x-b.x) + Abs(a.y-b.y)
+}
+
+func solve(rows, cols []int, galaxies []Point, expansion int) int {
+	galaxies_adjusted := make([]Point, len(galaxies))
+	copy(galaxies_adjusted, galaxies)
+
+	for i, g := range galaxies {
+		n_r := 0
+		for _, row := range rows {
+			if g.x > row {
+				n_r++
+			}
+		}
+		galaxies_adjusted[i].x += (expansion - 1) * n_r
+
+		n_c := 0
+		for _, col := range cols {
+			if g.y > col {
+				n_c++
+			}
+		}
+		galaxies_adjusted[i].y += (expansion - 1) * n_c
+	}
+
 	sum := 0
-	for i := range galaxies {
-		for j := i + 1; j < len(galaxies); j++ {
-			sum += ManhattanDistance(galaxies[i], galaxies[j], rows, cols, expansion-1)
+	for i := range galaxies_adjusted {
+		for j := i + 1; j < len(galaxies_adjusted); j++ {
+			sum += ManhattanDistance(galaxies_adjusted[i], galaxies_adjusted[j])
 		}
 	}
 	return sum
 }
 
-func Part1(rows, cols []int, galaxies [][]int) int {
+func Part1(rows, cols []int, galaxies []Point) int {
 	return solve(rows, cols, galaxies, 2)
 }
 
-func Part2(rows, cols []int, galaxies [][]int) int {
+func Part2(rows, cols []int, galaxies []Point) int {
 	return solve(rows, cols, galaxies, 1_000_000)
 }
 

735b985f058618947b68e9e085de99d2681f0422
Stefan Brandl <steve260897@gmail.com>
2023-12-16 08:43:05 +0100

    day16 in Python

--- day16/sol.py
+++ day16/sol.py
@@ -1,13 +1,15 @@
 from timing_util import Timing
 
 
-def get_data(data: str) -> list[list[str]]:
-    return [list(line) for line in data.split("\n")]
+def get_data(data: str) -> list[str]:
+    return data.split("\n")
 
 
-def follow_path(x: int, y: int, direction: str, already_visited: set[tuple[int, int, str]]):
-    # boundary check
-    while 0 <= y < len(data) and 0 <= x < len(data[y]):
+def follow_path(
+    data: list[str], x: int, y: int, direction: str, already_visited: set[tuple[int, int, str]]
+):
+    Y, X = len(data), len(data[0])
+    while 0 <= y < Y and 0 <= x < X:
         if (key := (x, y, direction)) in already_visited:
             return
         already_visited.add(key)
@@ -16,8 +18,8 @@ def follow_path(x: int, y: int, direction: str, already_visited: set[tuple[int, 
             case "right":
                 match data[y][x]:
                     case "|":
-                        follow_path(x, y - 1, "up", already_visited)
-                        follow_path(x, y + 1, "down", already_visited)
+                        follow_path(data, x, y - 1, "up", already_visited)
+                        follow_path(data, x, y + 1, "down", already_visited)
                     case "-":
                         x += 1
                     case "/":
@@ -31,8 +33,8 @@ def follow_path(x: int, y: int, direction: str, already_visited: set[tuple[int, 
             case "left":
                 match data[y][x]:
                     case "|":
-                        follow_path(x, y - 1, "up", already_visited)
-                        follow_path(x, y + 1, "down", already_visited)
+                        follow_path(data, x, y - 1, "up", already_visited)
+                        follow_path(data, x, y + 1, "down", already_visited)
                     case "-":
                         x -= 1
                     case "/":
@@ -48,8 +50,8 @@ def follow_path(x: int, y: int, direction: str, already_visited: set[tuple[int, 
                     case "|":
                         y -= 1
                     case "-":
-                        follow_path(x + 1, y, "right", already_visited)
-                        follow_path(x - 1, y, "left", already_visited)
+                        follow_path(data, x + 1, y, "right", already_visited)
+                        follow_path(data, x - 1, y, "left", already_visited)
                     case "/":
                         x += 1
                         direction = "right"
@@ -63,8 +65,8 @@ def follow_path(x: int, y: int, direction: str, already_visited: set[tuple[int, 
                     case "|":
                         y += 1
                     case "-":
-                        follow_path(x + 1, y, "right", already_visited)
-                        follow_path(x - 1, y, "left", already_visited)
+                        follow_path(data, x + 1, y, "right", already_visited)
+                        follow_path(data, x - 1, y, "left", already_visited)
                     case "/":
                         x -= 1
                         direction = "left"
@@ -75,15 +77,28 @@ def follow_path(x: int, y: int, direction: str, already_visited: set[tuple[int, 
                         y += 1
 
 
-def part1(data: list[list[str]]) -> int:
+def path_length(data: list[str], x: int, y: int, direction: str) -> int:
     already_visited = set()
-    follow_path(0, 0, "right", already_visited)
-    energized = set((x, y) for x, y, _ in already_visited)
-    return len(energized)
+    follow_path(data, x, y, direction, already_visited)
+    return len(set((x, y) for x, y, _ in already_visited))
 
 
-def part2(data: list[list[str]]) -> int:
-    return 0
+def part1(data: list[str]) -> int:
+    return path_length(data, 0, 0, "right")
+
+
+def part2(data: list[str]) -> int:
+    def stream_path_lengths(data: list[str]):
+        max_y, max_x = len(data) - 1, len(data[0]) - 1
+        for y in range(len(data)):
+            yield path_length(data, 0, y, "right")
+            yield path_length(data, max_x, y, "left")
+
+        for x in range(len(data[0])):
+            yield path_length(data, x, 0, "down")
+            yield path_length(data, x, max_y, "up")
+
+    return max(stream_path_lengths(data))
 
 
 if __name__ == "__main__":
